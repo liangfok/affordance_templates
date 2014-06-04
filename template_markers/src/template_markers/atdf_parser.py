eba5f89c51a3ccf894bdc6f4594d928ed2abf710
Steve Hart <stephen.hart@nasa.gov>
2014-06-04 17:01:08 -0500

    progress in xml parsing for "flat" structure of objects and waypoints

--- template_markers/src/template_markers/atdf_parser.py
+++ template_markers/src/template_markers/atdf_parser.py
@@ -3,29 +3,172 @@ import nasa_robot_teleop.xml_reflection.core as xmlr
 
 xmlr.start_namespace('atdf')
 
+xmlr.add_type('element_link', xmlr.SimpleElementType('link', str))
+xmlr.add_type('element_xyz', xmlr.SimpleElementType('xyz', 'vector3'))
 
-class DisplayObject(xmlr.Object):
-    def __init__(self, name = None):
-        self.aggregate_init()
-        self.name = name
+verbose = True
 
-xmlr.reflect(DisplayObject, params = [
-    xmlr.Attribute('name', str, False)
+class Pose(xmlr.Object):
+    def __init__(self, xyz=None, rpy=None):
+        self.xyz = xyz
+        self.rpy = rpy
+
+    def check_valid(self):
+        assert self.xyz is not None or self.rpy is not None
+
+    # Aliases for backwards compatibility
+    @property
+    def rotation(self): return self.rpy
+    @rotation.setter
+    def rotation(self, value): self.rpy = value
+    @property
+    def position(self): return self.xyz
+    @position.setter
+    def position(self, value): self.xyz = value
+
+xmlr.reflect(Pose, params = [
+    xmlr.Attribute('xyz', 'vector3', False),
+    xmlr.Attribute('rpy', 'vector3', False)
     ])
 
 
-# class DisplayObjects(xmlr.Object):
-#     def __init__(self, name = None):
-#         self.aggregate_init()
-#         self.objects = []
+# Common stuff
+name_attribute = xmlr.Attribute('name', str)
+origin_element = xmlr.Element('origin', Pose, False)
 
-# xmlr.reflect(DisplayObjects, params = [
-#     xmlr.Element('display_object', DisplayObject)
-#     ])
+class Color(xmlr.Object):
+    def __init__(self, *args):
+        # What about named colors?
+        count = len(args)
+        if count == 4 or count == 3:
+            self.rgba = args
+        elif count == 1:
+            self.rgba = args[0]
+        elif count == 0:
+            self.rgba = None
+        if self.rgba is not None:
+            if len(self.rgba) == 3:
+                self.rgba += [1.]
+            if len(self.rgba) != 4:
+                raise Exception('Invalid color argument count')
+
+xmlr.reflect(Color, params = [
+    xmlr.Attribute('rgba', 'vector4')
+    ])
+
+
+class Box(xmlr.Object):
+    def __init__(self, size = None):
+        self.size = size
+
+xmlr.reflect(Box, params = [
+    xmlr.Attribute('size', 'vector3')
+    ])
+
+
+class Cylinder(xmlr.Object):
+    def __init__(self, radius = 0.0, length = 0.0):
+        self.radius = radius
+        self.length = length
+
+xmlr.reflect(Cylinder, params = [
+    xmlr.Attribute('radius', float),
+    xmlr.Attribute('length', float)
+    ])
+
+
+class Sphere(xmlr.Object):
+    def __init__(self, radius=0.0):
+        self.radius = radius
+
+xmlr.reflect(Sphere, params = [
+    xmlr.Attribute('radius', float)
+    ])
+
+
+class Mesh(xmlr.Object):
+    def __init__(self, filename = None, scale = None):
+        self.filename = filename
+        self.scale = scale
+
+xmlr.reflect(Mesh, params = [
+    xmlr.Attribute('filename', str),
+    xmlr.Attribute('scale', 'vector3', required=False)
+    ])
+
+class Texture(xmlr.Object):
+    def __init__(self, filename = None):
+        self.filename = filename
+
+xmlr.reflect(Texture, params = [
+    xmlr.Attribute('filename', str)
+    ])
+
+
+class Material(xmlr.Object):
+    def __init__(self, name=None, color=None, texture=None):
+        self.name = name
+        self.color = color
+        self.texture = texture
+
+    def check_valid(self):
+        if self.color is None and self.texture is None:
+            # xmlr.on_error("Material has neither a color nor texture\n")
+            pass
+
+xmlr.reflect(Material, params = [
+    name_attribute,
+    xmlr.Element('color', Color, False),
+    xmlr.Element('texture', Texture, False)
+    ])
+
+
+class GeometricType(xmlr.ValueType):
+    def __init__(self):
+        self.factory = xmlr.FactoryType('geometric', {
+            'box': Box,
+            'cylinder': Cylinder,
+            'sphere': Sphere,
+            'mesh': Mesh
+            })
+
+    def from_xml(self, node):
+        children = xml_children(node)
+        assert len(children) == 1, 'One element only for geometric'
+        return self.factory.from_xml(children[0])
+
+    def write_xml(self, node, obj):
+        name = self.factory.get_name(obj)
+        child = node_add(node, name)
+        obj.write_xml(child)
+
+xmlr.add_type('geometric', GeometricType())
+
+
+class DisplayObject(xmlr.Object):
+    def __init__(self, name = None, geometry = None, origin = None, material = None):
+        self.name = name
+        self.origin = origin
+        self.geometry = geometry
+        self.material = material
+
+xmlr.reflect(DisplayObject, params = [
+    origin_element,
+    xmlr.Element('geometry', 'geometric'),
+    xmlr.Element('material', Material, False),
+    xmlr.Attribute('name', str, True)
+    ])
+
+class DisplayObjects(xmlr.Object):
+    def __init__(self, name = None):
+        self.objects = []
+
+xmlr.reflect(DisplayObjects, params = [
+    xmlr.Element('display_object', DisplayObject)
+    ])
 
 # class EndEffectorWaypoint(xmlr.Object):
 #     def __init__(self, id = None):
-#         self.aggregate_init()
 #         self.id = id
 
 # xmlr.reflect(EndEffectorWaypoint, params = [
@@ -34,7 +177,6 @@ xmlr.reflect(DisplayObject, params = [
 
 # class EndEffectorWaypoints(xmlr.Object):
 #     def __init__(self):
-#         self.aggregate_init()
 #         self.waypoints = []
 
 # xmlr.reflect(EndEffectorWaypoints, params = [
@@ -46,35 +188,27 @@ class AffordanceTemplate(xmlr.Object):
         self.aggregate_init()
 
         self.name = name
-        self.objects = []
-        self.waypoints = []
+        self.display_objects = []
+        self.end_effector_waypoints = []
 
-        self.object_map = {}
-        self.waypoint_map = {}
-
-        # self.parent_map = {}
-        # self.child_map = {}
+        self.display_object_map = {}
+        self.end_effector_waypoint_map = {}
 
     def add_aggregate(self, typeName, elem):
         xmlr.Object.add_aggregate(self, typeName, elem)
 
-        if typeName == 'object':
-            obj = elem
-            self.object_map[obj.name] = obj
-            # self.parent_map[ joint.child ] = (joint.name, joint.parent)
-            # if joint.parent in self.child_map:
-            #     self.child_map[joint.parent].append( (joint.name, joint.child) )
-            # else:
-            #     self.child_map[joint.parent] = [ (joint.name, joint.child) ]
-        elif typeName == 'waypoint':
-            waypoint = elem
-            self.waypoint_map[waypoint.name] = waypoint
+        if typeName == 'display_object':
+            display_object = elem
+            self.display_object_map[display_object.name] = display_object
+        elif typeName == 'end_effector_waypoint':
+            end_effector_waypoint = elem
+            self.end_effector_waypoint_map[end_effector_waypoint.name] = end_effector_waypoint
 
-    def add_object(self, obj):
-        self.add_aggregate('object', obj)
+    def add_display_object(self, display_object):
+        self.add_aggregate('display_object', display_object)
 
-    def add_waypoint(self, waypoint):
-        self.add_aggregate('waypoint', waypoint)
+    def add_end_effector_waypoint(self, end_effector_waypoint):
+        self.add_aggregate('end_effector_waypoint', waypoint)
 
     @classmethod
     def from_file(cls, key = 'template_description'):
@@ -89,7 +223,7 @@ xmlr.reflect(AffordanceTemplate, tag = 'template', params = [
 #   name_attribute,
     xmlr.Attribute('name', str, True),
     xmlr.Attribute('image', str, False),
-    xmlr.AggregateElement('display_object', DisplayObject),
+    xmlr.AggregateElement('display_object', DisplayObject)
     # xmlr.Attribute('end_effector_waypoints', EndEffectorWaypoints)
     ])
 
